163a2b4d42942db931c72e39e8f36499a9a8ef26
maturelion <maturelion1@gmail.com>
2024-08-22 21:38:18 +0100

    Fix QR Code display

--- src/components/Receive/Receive.js
+++ src/components/Receive/Receive.js
@@ -1,95 +1,138 @@
 import React, { useState } from "react";
 import {
-  AddressCard,
-  ReceiveContainer,
-  ReceiveTab,
-  ReceiveTabContainer,
+    AddressCard,
+    ReceiveContainer,
+    ReceiveTab,
+    ReceiveTabContainer,
 } from "./Receive.styled";
 import { BackArrow } from "../Components.styled";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import QRCode from "react-qr-code";
 
 const Receive = () => {
-  const navigate = useNavigate();
-  const [tabs, setTabs] = useState("BTC");
-  let depositAddress = "";
-  let network = "";
-  switch (tabs) {
-    case "BTC":
-      depositAddress = "bc1qdwp8t00zy6jawj3e9t0d60e09a2w0xalgfmclt";
-      network = "Bitcoin"
-      break;
-    case "ETH":
-      depositAddress = "0x44A0bDbeC7b3BcFc0b2efc9Bd3400D76568FC8Ba";
-      network = "ERC20"
-      break;
-    case "USDT":
-      depositAddress = "TXkBH766HJ3TWvJ9sDxHohH9rQML2vM9D8";
-      network = "TRC20"
-      break;
+    const navigate = useNavigate();
+    const [tabs, setTabs] = useState("BTC");
+    let depositAddress = "";
+    let network = "";
+    switch (tabs) {
+        case "BTC":
+            depositAddress = "bc1qdwp8t00zy6jawj3e9t0d60e09a2w0xalgfmclt";
+            network = "Bitcoin";
+            break;
+        case "ETH":
+            depositAddress = "0x44A0bDbeC7b3BcFc0b2efc9Bd3400D76568FC8Ba";
+            network = "ERC20";
+            break;
+        case "USDT":
+            depositAddress = "TXkBH766HJ3TWvJ9sDxHohH9rQML2vM9D8";
+            network = "TRC20";
+            break;
 
-    default:
-      break;
-  }
+        default:
+            break;
+    }
 
-  return (
-    <>
-      <h2
-        style={{
-          position: "absolute",
-          width: "100vw",
-          textAlign: "center",
-          zIndex: "-1",
-        }}
-      >
-        Receive
-      </h2>
-      <BackArrow style={{ marginInlineStart: "10px" }}>
-        <FontAwesomeIcon
-          style={{ cursor: "pointer" }}
-          icon={faArrowLeftLong}
-          onClick={() => navigate(-1)}
-        />
-      </BackArrow>
-      <ReceiveContainer>
-        <AddressCard>
-          <ReceiveTabContainer>
-            <ReceiveTab style={{textDecoration: `${tabs === "BTC" ? "underline" : ""}`}} onClick={() => setTabs("BTC")}>BTC</ReceiveTab>
-            <ReceiveTab style={{textDecoration: `${tabs === "ETH" ? "underline" : ""}`}} onClick={() => setTabs("ETH")}>ETH</ReceiveTab>
-            <ReceiveTab style={{textDecoration: `${tabs === "USDT" ? "underline" : ""}`}} onClick={() => setTabs("USDT")}>USDT</ReceiveTab>
-          </ReceiveTabContainer>
-          <>
-            <img
-              style={{
-                height: "250px",
-                width: "250px",
-                backgroundColor: "#fff",
-                borderRadius: "10px",
-              }}
-              src={`https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=${depositAddress}&choe=UTF-8`}
-              alt="qr"
-            />
-            <div>Network: {tabs}({network})</div>
-            {depositAddress}
-            <div
-              style={{ marginBlockStart: "80px", color: "#1652F0" }}
-              onClick={() => {
-                navigator.clipboard
-                  .writeText(depositAddress)
-                  .then(() => {
-                    alert("Deposit address copied to clipboard");
-                  })
-                  .catch(console.error);
-              }}
+    return (
+        <>
+            <h2
+                style={{
+                    position: "absolute",
+                    width: "100vw",
+                    textAlign: "center",
+                    zIndex: "-1",
+                }}
             >
-              Copy or share address
-            </div>
-          </>
-        </AddressCard>
-      </ReceiveContainer>
-    </>
-  );
+                Receive
+            </h2>
+            <BackArrow style={{ marginInlineStart: "10px" }}>
+                <FontAwesomeIcon
+                    style={{ cursor: "pointer" }}
+                    icon={faArrowLeftLong}
+                    onClick={() => navigate(-1)}
+                />
+            </BackArrow>
+            <ReceiveContainer>
+                <AddressCard>
+                    <ReceiveTabContainer>
+                        <ReceiveTab
+                            style={{
+                                textDecoration: `${
+                                    tabs === "BTC" ? "underline" : ""
+                                }`,
+                            }}
+                            onClick={() => setTabs("BTC")}
+                        >
+                            BTC
+                        </ReceiveTab>
+                        <ReceiveTab
+                            style={{
+                                textDecoration: `${
+                                    tabs === "ETH" ? "underline" : ""
+                                }`,
+                            }}
+                            onClick={() => setTabs("ETH")}
+                        >
+                            ETH
+                        </ReceiveTab>
+                        <ReceiveTab
+                            style={{
+                                textDecoration: `${
+                                    tabs === "USDT" ? "underline" : ""
+                                }`,
+                            }}
+                            onClick={() => setTabs("USDT")}
+                        >
+                            USDT
+                        </ReceiveTab>
+                    </ReceiveTabContainer>
+                    <>
+                        <div
+                            style={{
+                                height: "250px",
+                                width: "250px",
+                                backgroundColor: "#fff",
+                                borderRadius: "10px",
+                                padding: "10px"
+                            }}
+                        >
+                            <QRCode
+                                style={{
+                                    height: "auto",
+                                    maxWidth: "100%",
+                                    width: "100%",
+                                }}
+                                value={`${depositAddress}`}
+                            />
+                        </div>
+                        <div>
+                            Network: {tabs}({network})
+                        </div>
+                        {depositAddress}
+                        <div
+                            style={{
+                                marginBlockStart: "80px",
+                                color: "#1652F0",
+                            }}
+                            onClick={() => {
+                                navigator.clipboard
+                                    .writeText(depositAddress)
+                                    .then(() => {
+                                        alert(
+                                            "Deposit address copied to clipboard"
+                                        );
+                                    })
+                                    .catch(console.error);
+                            }}
+                        >
+                            Copy or share address
+                        </div>
+                    </>
+                </AddressCard>
+            </ReceiveContainer>
+        </>
+    );
 };
 
 export default Receive;
